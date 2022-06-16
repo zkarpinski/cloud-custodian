9eff14c14f49f5980aa4dba6f571e5707c8a44a7
Kiran Moka <kiran29.moka@gmail.com>
2022-06-16 20:24:21 +0530

    aws - cloudsearch - add domain-options filter and enable-https action (#7280)

--- tests/test_cloudsearch.py
+++ tests/test_cloudsearch.py
@@ -1,7 +1,7 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 
-from .common import BaseTest
+from .common import BaseTest, event_data
 
 
 class CloudSearch(BaseTest):
@@ -38,3 +38,53 @@ class CloudSearch(BaseTest):
             0
         ]
         self.assertEqual(state["Deleted"], True)
+
+    def test_enable_https_cloud_search(self):
+        """ CloudSearchEnableHttpsTest: tes_enable_https_cloud_search: enable https """
+        session_factory = self.replay_flight_data("test_enable_https_cloud_search")
+        p = self.load_policy(
+            {
+                "name": "cloudsearch-enable-https",
+                "resource": "cloudsearch",
+                "mode": {
+                    "type": "cloudtrail",
+                    "events": [
+                        {
+                            "event": "CreateDomain",
+                            "source": "cloudsearch.amazonaws.com",
+                            "ids": "requestParameters.domainName"
+                        }
+                    ]
+                },
+                "actions": [
+                    {
+                        "type": "enable-https",
+                        "tls-policy": "Policy-Min-TLS-1-2-2019-07"
+                    }
+                ],
+                "filters": [{
+                    "type": "domain-options",
+                    "key": "Options.EnforceHTTPS",
+                    "value": False,
+                }]
+            },
+            session_factory=session_factory
+        )
+        event = event_data("event-cloudsearch.json", "config")
+        resources = p.push(event, {})
+        client = session_factory().client('cloudsearch')
+        for resource in resources:
+            self.assert_cloudsearch_https(client, resource)
+
+    def assert_cloudsearch_https(self, client, resource):
+        """
+        Tests that https flag set to true
+        Args:
+            resource: cloudsearch resource
+            client (obj): aws cloudsearch client
+        """
+        domain_name = resource['DomainName']
+        response = client.describe_domain_endpoint_options(
+            DomainName=domain_name)
+        https_status = response['DomainEndpointOptions']['Options']['EnforceHTTPS']
+        self.assertEqual(https_status, True, 'cloud search https is enabled')
