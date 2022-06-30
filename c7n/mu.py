30ed353f94ec31d4a43974eb3ba48539a0f843d1
Darren Dao <darrendao@gmail.com>
2022-06-30 06:01:55 -0700

    aws - mu - add waiter to lambda creation to support aws lamdba states (#7539)

--- c7n/mu.py
+++ c7n/mu.py
@@ -458,7 +458,6 @@ class LambdaManager:
         assert role, "Lambda function role must be specified"
         archive = func.get_archive()
         existing = self.get(func.name, qualifier)
-        function_updated_waiter = self.client.get_waiter('function_updated')
 
         if s3_uri:
             # TODO: support versioned buckets
@@ -475,7 +474,8 @@ class LambdaManager:
                 params = dict(FunctionName=func.name, Publish=True)
                 params.update(code_ref)
                 result = self.client.update_function_code(**params)
-                function_updated_waiter.wait(FunctionName=func.name)
+                waiter = self.client.get_waiter('function_updated')
+                waiter.wait(FunctionName=func.name)
                 changed = True
 
             # TODO/Consider also set publish above to false, and publish
@@ -501,6 +501,8 @@ class LambdaManager:
             params.update({'Publish': True, 'Code': code_ref, 'Role': role})
             result = self.client.create_function(**params)
             self._update_concurrency(None, func)
+            waiter = self.client.get_waiter('function_active')
+            waiter.wait(FunctionName=func.name)
             changed = True
 
         return result, changed
