a54e95d0c8a5cca6fc2be0d3ac7c2fa1b962d360
Naohito Takeuchi <104223034+naohito-intuit@users.noreply.github.com>
2022-07-20 07:35:34 -0700

    aws - vpc - flow-logs - fix LogDestination key error (#7569)

--- c7n/resources/vpc.py
+++ c7n/resources/vpc.py
@@ -134,6 +134,8 @@ class FlowLogFilter(Filter):
                 for fl in flogs:
                     dest_type_match = (destination_type is None) or op(
                         fl['LogDestinationType'], destination_type)
+                    if 'LogDestination' not in fl:
+                        fl['LogDestination'] = ''
                     dest_match = (destination is None) or op(
                         fl['LogDestination'], destination)
                     status_match = (status is None) or op(fl['FlowLogStatus'], status.upper())
