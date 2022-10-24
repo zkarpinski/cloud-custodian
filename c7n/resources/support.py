d93ef2ca53b327ae2e0ac225320ccdfe75936ead
Kent Ou <84220825+kentnsw@users.noreply.github.com>
2022-10-24 22:25:34 +1100

    aws - support-case - use helper to get correct region per partition (#7927)

--- c7n/resources/support.py
+++ c7n/resources/support.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from c7n.manager import resources
 from c7n.query import QueryResourceManager, TypeInfo
+from c7n.utils import local_session, get_support_region
 
 
 @resources.register('support-case')
@@ -17,3 +18,7 @@ class SupportCase(QueryResourceManager):
         name = 'displayId'
         date = 'timeCreated'
         arn = False
+
+    def get_client(self):
+        region = get_support_region(self)
+        return local_session(self.session_factory).client('support', region_name=region)
