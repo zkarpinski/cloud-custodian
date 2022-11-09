01dbdbb7262791c8d99917e58b144413a0a90d96
Derek Slagle <derek_slagle@mckinsey.com>
2022-11-09 13:44:50 -0500

    azure - postgresql-server - add configuration-parameter filter (#7876)

--- tools/c7n_azure/c7n_azure/resources/postgresql_server.py
+++ tools/c7n_azure/c7n_azure/resources/postgresql_server.py
@@ -1,6 +1,8 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 
+from c7n.filters.core import ValueFilter
+from c7n.utils import type_schema
 from c7n_azure.provider import resources
 from c7n_azure.resources.arm import ArmResourceManager
 from c7n_azure.filters import FirewallRulesFilter
@@ -69,3 +71,69 @@ class PostgresqlServerFirewallRulesFilter(FirewallRulesFilter):
                 continue
             resource_rules.add(rule)
         return resource_rules
+
+
+@PostgresqlServer.filter_registry.register('configuration-parameter')
+class ConfigurationParametersFilter(ValueFilter):
+    """Filter by configuration parameter for this postresql server
+
+    Configurations are made available to the filter as a map with each
+    key holding the name of the configuration and each value holding
+    the properties of the Configuration as defined here:
+    https://learn.microsoft.com/en-us/python/api/azure-mgmt-rdbms/azure.mgmt.rdbms.postgresql.models.configuration?view=azure-python
+
+    :example:
+
+    Example JSON document showing the data format provided to the filter
+
+    .. code-block:: json
+
+      {
+        "value": "off",
+        "description": "Logs each successful connection.",
+        "defaultValue": "on",
+        "dataType": "Boolean",
+        "allowedValues": "on,off",
+        "source": "user-override",
+        "isConfigPendingRestart": "False",
+        "isDynamicConfig": "True"
+      }
+
+    :example:
+
+    Find Postgresql servers with log_connections not enabled
+
+    .. code-block:: yaml
+
+        policies:
+          - name: sql-database-no-log-connections
+            resource: azure.postgresql-server
+            filters:
+              - type: configuration-parameter
+                name: log_connections
+                key: value
+                op: ne
+                value: 'on'
+
+    """
+
+    schema = type_schema(
+        'configuration-parameter',
+        required=['type', 'name'],
+        rinherit=ValueFilter.schema,
+        name=dict(type='string')
+    )
+
+    def __call__(self, resource):
+        key = f'c7n:config-params:{self.data["name"]}'
+        if key not in resource['properties']:
+            client = self.manager.get_client()
+            query = client.configurations.get(
+                resource['resourceGroup'],
+                resource['name'],
+                self.data["name"]
+            )
+
+            resource['properties'][key] = query.serialize(True).get('properties')
+
+        return super().__call__(resource['properties'][key])
--- tools/c7n_azure/tests_azure/tests_resources/test_postgresql_server.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_postgresql_server.py
@@ -1,10 +1,12 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 import collections
+from unittest.mock import call, Mock
 from netaddr import IPSet
-from mock import Mock
 from ..azure_common import BaseTest, arm_template
-from c7n_azure.resources.postgresql_server import PostgresqlServerFirewallRulesFilter
+from c7n_azure.resources.postgresql_server import \
+    ConfigurationParametersFilter, \
+    PostgresqlServerFirewallRulesFilter
 
 IpRange = collections.namedtuple('IpRange', 'start_ip_address end_ip_address')
 
@@ -72,3 +74,137 @@ class PostgresqlServerFirewallFilterTest(BaseTest):
         filter.client = Mock()
         filter.client.firewall_rules.list_by_server.return_value = rules
         return filter
+
+
+class PostgresqlConfigurationParameterFilterTest(BaseTest):
+    # one parameter configured in the ARM template with log_connections = 'off'
+    @arm_template('postgresql.json')
+    def test_server_configuration_parameter(self):
+        p = self.load_policy({
+            'name': 'test-azure-postgresql-server-configurations',
+            'resource': 'azure.postgresql-server',
+            'filters': [
+                {
+                    'type': 'configuration-parameter',
+                    'name': 'log_connections',
+                    'key': 'value',
+                    'op': 'ne',
+                    # 'key': "log_connections.value != 'on' or log_disconnections.value != 'on'",
+                    'value': 'on'
+                }
+            ],
+        })
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+    def test_int_value_with_regex(self):
+        resources = self._get_test_resources()
+        data = self._gen_filter_data(123, value_regex='test-(\\d+)', value_type='integer')
+        mock_parameter = dict(properties=dict(value='test-123'))
+
+        filter = self._get_filter(data, mock_parameter)
+        actual = filter.process(resources)
+
+        # ensure we call azure with the correct resource information
+        filter.manager.get_client().configurations.get.assert_called_once_with(
+            'test-group-1',
+            'test-name-1',
+            'test-param'
+        )
+        self.assertListEqual(resources, actual)
+
+    def test_azure_api_called_only_once_per_resource(self):
+        resources = self._get_test_resources(2)
+
+        data1 = self._gen_filter_data('123')
+        data2 = self._gen_filter_data('456')
+        mock_parameter = dict(properties=dict(value='test-123'))
+
+        filter1 = self._get_filter(data1, mock_parameter)
+        filter2 = self._get_filter(data2, mock_parameter)
+
+        # result is unimportant for this test
+        # also values are cached on the resource instance so calling two filters on the same
+        # resources should not result in additional API calls
+        filter1.process(resources)
+        filter2.process(resources)
+
+        # ensure we call azure with the correct resource information - and only once per
+        # resource even if there are two filters (ensure the cached values are used)
+        filter1.manager.get_client().configurations.get.assert_has_calls([
+            call('test-group-1', 'test-name-1', 'test-param'),
+            call('test-group-2', 'test-name-2', 'test-param')
+        ])
+        filter2.manager.get_client().configurations.get.assert_not_called()
+
+    def test_date_value(self):
+        resources = self._get_test_resources()
+        data = self._gen_filter_data('1/1/2023', op='lt', value_type='date')
+        # note - str compare would be false
+        mock_parameter = dict(properties=dict(value='5/1/2022'))
+
+        filter = self._get_filter(data, mock_parameter)
+        actual = filter.process(resources)
+
+        self.assertListEqual(resources, actual)
+
+    def test_all_resources_passing_with_float(self):
+        resources = self._get_test_resources()
+        data = self._gen_filter_data('2.5', op='gt', value_type='float')
+        mock_parameter = dict(properties=dict(value='1.5'))
+
+        filter = self._get_filter(data, mock_parameter)
+        actual = filter.process(resources)
+
+        self.assertEqual(0, len(actual))
+
+    def test_list_op_no_match(self):
+        resources = self._get_test_resources()
+        data = self._gen_filter_data(['1', '2', '3', '4'], op='in')
+        mock_parameter = dict(properties=dict(value='5'))
+
+        filter = self._get_filter(data, mock_parameter)
+        actual = filter.process(resources)
+
+        self.assertEqual(0, len(actual))
+
+    def test_list_op_matching(self):
+        resources = self._get_test_resources()
+        data = self._gen_filter_data(['1', '2', '3', '4'], op='in')
+        mock_parameter = dict(properties=dict(value='4'))
+
+        filter = self._get_filter(data, mock_parameter)
+        actual = filter.process(resources)
+
+        self.assertListEqual(resources, actual)
+
+    def _get_test_resources(self, count=1):
+        return [
+            dict(name=f'test-name-{i+1}', resourceGroup=f'test-group-{i+1}', properties={})
+            for i in range(count)
+        ]
+
+    def _get_filter(self, data, configurations):
+        client = Mock()
+        client.configurations.get = Mock(
+            return_value=Mock(
+                serialize=Mock(return_value=configurations)
+            )
+        )
+
+        manager = Mock()
+        manager.get_client = Mock(return_value=client)
+
+        return ConfigurationParametersFilter(data, manager=manager)
+
+    def _gen_filter_data(self, value, op='eq', value_regex=None, value_type=None,
+            name='test-param'):
+        return dict(
+            type='configuration-parameter',
+            key='value',
+            name=name,
+            value=value,
+            op=op,
+            **(dict(value_regex=value_regex) if value_regex else {}),
+            **(dict(value_type=value_type) if value_type else {}),
+        )
