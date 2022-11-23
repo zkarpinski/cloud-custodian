3743994e518bcfe61168fff1b26ee0e5f1b88c07
Kapil Thangavelu <kapilt@gmail.com>
2022-11-23 14:52:51 -0500

    releng - update poetry to 1.2.2 (#8013)

--- tools/dev/dockerpkg.py
+++ tools/dev/dockerpkg.py
@@ -65,7 +65,7 @@ BUILD_STAGE = """\
 
 FROM {base_build_image} as build-env
 
-ARG POETRY_VERSION="1.2.1"
+ARG POETRY_VERSION="1.2.2"
 SHELL ["/bin/bash", "-c"]
 
 # pre-requisite distro deps, and build env setup
