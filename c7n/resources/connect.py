12d2559975d744b03f0afb4df355cd5befbc45a9
timmygrable <59906410+timmygrable@users.noreply.github.com>
2023-01-10 14:03:56 -0500

    aws - connect - add set-attribute action (#8095)

--- c7n/resources/connect.py
+++ c7n/resources/connect.py
@@ -4,6 +4,7 @@ from c7n.manager import resources
 from c7n.query import QueryResourceManager, TypeInfo
 from c7n.filters import ValueFilter
 from c7n.utils import local_session, type_schema
+from c7n.actions import Action
 
 
 @resources.register('connect-instance')
@@ -59,3 +60,51 @@ class ConnectInstanceAttributeFilter(ValueFilter):
                 results.append(r)
 
         return results
+
+    @Connect.action_registry.register("set-attributes")
+    class SetAttributes(Action):
+        """Set the attributes for the connect resources
+
+        :example:
+
+        .. code-block:: yaml
+
+            policies:
+              - name: connect-set-contact-lens
+                resource: connect-instance
+                filters:
+                  - type: instance-attribute
+                    key: Attribute.Value
+                    value: false
+                    attribute_type: CONTACT_LENS
+                actions:
+                  - type: set-attributes
+                    attribute_type: CONTACT_LENS
+                    value: true
+              - name: connect-disable-contact-lens
+                resource: connect-instance
+                filters:
+                  - type: instance-attribute
+                    key: Attribute.Value
+                    value: true
+                    attribute_type: CONTACT_LENS
+                actions:
+                  - type: set-attributes
+                    attribute_type: CONTACT_LENS
+                    value: false
+        """
+        attributes = ["INBOUND_CALLS", "OUTBOUND_CALLS",
+                      "CONTACTFLOW_LOGS", "CONTACT_LENS",
+                      "AUTO_RESOLVE_BEST_VOICES", "USE_CUSTOM_TTS_VOICES",
+                      "EARLY_MEDIA", "MULTI_PARTY_CONFERENCE",
+                      "HIGH_VOLUME_OUTBOUND", "ENHANCED_CONTACT_MONITORING"]
+        schema = type_schema("set-attributes", attribute_type={'anyOf': [{'enum': attributes},
+                  {'type': 'string'}]}, value={}, required=["value", "attribute_type"])
+        permissions = ("connect:UpdateInstanceAttribute",)
+
+        def process(self, resources):
+            client = local_session(self.manager.session_factory).client('connect')
+
+            for r in resources:
+                client.update_instance_attribute(InstanceId=r["Id"],
+                    AttributeType=self.data.get("attribute_type"), Value=self.data.get("value"))
--- tests/test_connect.py
+++ tests/test_connect.py
@@ -33,3 +33,68 @@ class ConnectTest(BaseTest):
         )
         resources = p.run()
         self.assertEqual(len(resources), 1)
+
+    def test_connect_set_attributes_true(self):
+        session_factory = self.replay_flight_data("test_connect_set_attributes_true")
+        p = self.load_policy(
+            {
+                "name": "connect-instance-set-contact-lens",
+                "resource": "connect-instance",
+                "filters": [{
+                    'type': 'instance-attribute',
+                    'key': 'Attribute.Value',
+                    'value': 'false',
+                    'attribute_type': 'CONTACT_LENS'
+                }],
+                "actions": [
+                    {'type': 'set-attributes',
+                    "attribute_type": "CONTACT_LENS",
+                    "value": "true"}
+                ]
+            },
+            session_factory=session_factory
+        )
+        resources = p.run()
+        results = []
+        for r in resources:
+            results.append(
+                session_factory().client('connect').describe_instance_attribute(
+                    InstanceId=r["Id"],
+                    AttributeType=r["c7n:InstanceAttribute"]["Attribute"]["AttributeType"])
+            )
+
+        self.assertEqual(results[0]["Attribute"]["AttributeType"], "CONTACT_LENS")
+        self.assertEqual(results[0]["Attribute"]["Value"], "true")
+        self.assertEqual(len(resources), 1)
+
+    def test_connect_set_attributes_false(self):
+        session_factory = self.replay_flight_data("test_connect_set_attributes_false")
+        p = self.load_policy(
+            {
+                "name": "connect-instance-disable-contact-lens-test",
+                "resource": "connect-instance",
+                "filters": [{
+                    'type': 'instance-attribute',
+                    'key': 'Attribute.Value',
+                    'value': 'true',
+                    'attribute_type': 'CONTACT_LENS'
+                }],
+                "actions": [
+                    {'type': 'set-attributes',
+                    "attribute_type": "CONTACT_LENS",
+                    "value": "false"}
+                ]
+            },
+            session_factory=session_factory
+        )
+        resources = p.run()
+        results = []
+        for r in resources:
+            results.append(
+                session_factory().client('connect').describe_instance_attribute(
+                    InstanceId=r["Id"],
+                    AttributeType=r["c7n:InstanceAttribute"]["Attribute"]["AttributeType"])
+            )
+        self.assertEqual(results[0]["Attribute"]["AttributeType"], "CONTACT_LENS")
+        self.assertEqual(results[0]["Attribute"]["Value"], "false")
+        self.assertEqual(len(resources), 1)
