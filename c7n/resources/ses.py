eaba7c605d6e42934d612ecc28d6f4ec62ae5845
Ryan Ahlgren <52678187+ahlgreno@users.noreply.github.com>
2023-06-13 13:03:07 -0500

    aws - ses - add set-delivery-options action (#8635)

--- c7n/resources/ses.py
+++ c7n/resources/ses.py
@@ -1,9 +1,9 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
-
+from c7n.actions import BaseAction
 from c7n.manager import resources
 from c7n.query import DescribeSource, QueryResourceManager, TypeInfo
-from c7n.utils import local_session
+from c7n.utils import local_session, type_schema
 from c7n.tags import universal_augment
 
 class DescribeConfigurationSet(DescribeSource):
@@ -34,3 +34,42 @@ class SESConfigurationSet(QueryResourceManager):
         'describe': DescribeConfigurationSet
     }
 
+
+@SESConfigurationSet.action_registry.register('set-delivery-options')
+class SetDeliveryOptions(BaseAction):
+    """Set the TLS policy for ses
+
+    :example:
+
+    .. code-block:: yaml
+
+            policies:
+              - name: ses-set-delivery-options-require
+                resource: ses-configuration-set
+                filters:
+                  - type: value
+                    key: DeliveryOptions.TlsPolicy
+                    op: eq
+                    value: Optional
+                actions:
+                  - type: set-delivery-options
+                    tls-policy: Require
+    """
+
+    schema = type_schema(
+        'set-delivery-options',
+        required=['tls-policy'],
+        **{"tls-policy": {'enum': ['Require', 'Optional']}},
+    )
+    permissions = ("ses:PutConfigurationSetDeliveryOptions",)
+
+    def process(self, resources):
+        client = local_session(self.manager.session_factory).client('ses')
+        tls_policy = self.data.get('tls-policy')
+        for r in resources:
+            client.put_configuration_set_delivery_options(
+                ConfigurationSetName=r['Name'],
+                DeliveryOptions={
+                    'TlsPolicy': tls_policy
+                },
+            )
--- tests/test_ses.py
+++ tests/test_ses.py
@@ -14,3 +14,30 @@ class SESTest(BaseTest):
         )
         resources = p.run()
         self.assertEqual(len(resources), 1)
+
+    def test_ses_configuration_set_delivery_options(self):
+        session_factory = self.replay_flight_data("test_ses_configuration_set_delivery_options")
+        p = self.load_policy(
+            {
+                "name": "ses-configuration-set-delivery-options-test",
+                "resource": "ses-configuration-set",
+                "filters": [{"type": "value",
+                             "key": "DeliveryOptions.TlsPolicy",
+                             "op": "eq",
+                             "value": "Optional"},
+                            ],
+                "actions": [
+                    {
+                        "type": "set-delivery-options",
+                        "tls-policy": "Require"
+                    }
+                ]
+            }, session_factory=session_factory
+        )
+        resources = p.run()
+        client = session_factory().client('ses')
+        for r in resources:
+            response = client.describe_configuration_set(ConfigurationSetName=r["Name"],
+                                                         ConfigurationSetAttributeNames=['deliveryOptions'])
+            tls_policy = response['DeliveryOptions']['TlsPolicy']
+            self.assertEqual(tls_policy, "Require")
