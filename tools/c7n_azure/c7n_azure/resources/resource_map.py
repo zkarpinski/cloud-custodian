a47d9393b802b8e80c8d3881ef13df78b8d5981d
Afanasiev Dmytro <57749020+dmytro-afanasiev@users.noreply.github.com>
2023-10-16 17:54:22 +0300

    azure - signalr resource (#9062)

--- tools/c7n_azure/c7n_azure/resources/resource_map.py
+++ tools/c7n_azure/c7n_azure/resources/resource_map.py
@@ -73,6 +73,7 @@ ResourceMap = {
     "azure.service-fabric-cluster": "c7n_azure.resources.service_fabric.ServiceFabricCluster",
     "azure.service-fabric-cluster-managed": "c7n_azure.resources.service_fabric.ServiceFabricClusterManaged",  # noqa
     "azure.session-host": "c7n_azure.resources.session_host.SessionHost",
+    "azure.signalr": "c7n_azure.resources.signalr.Signalr",
     "azure.spring-app": "c7n_azure.resources.spring.SpringApp",
     "azure.spring-service-instance": "c7n_azure.resources.spring.SpringServiceInstance",
     "azure.sql-database": "c7n_azure.resources.sqldatabase.SqlDatabase",
